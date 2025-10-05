5e5ae4054fc2ce6c165f74760569137a21ab2e88
Igor Calandrine <igorwow@hotmail.com>
2025-10-05 13:27:18 -0300

    Detail ajustmants
    a

--- 4-Aninhadas.py
+++ 4-Aninhadas.py
@@ -1,8 +1,9 @@
-"""Informações aninhadas
-Às vezes, você vai querer armazenar um conjunto de dicionários em uma lista  ou  uma  lista  de  itens  como  um  valor  em  um  dicionário.  Isso  é conhecido como aninhar informações. Podemos aninhar um conjunto de dicionários  em  uma  lista,  uma  lista  de  itens  em  um  dicionário  ou  até mesmo  um  dicionário  em  outro  dicionário. 
+"""
+Informações aninhadas
+    Às vezes, você vai querer armazenar um conjunto de dicionários em uma lista  ou  uma  lista  de  itens  como  um  valor  em  um  dicionário.  Isso  é conhecido como aninhar informações. Podemos aninhar um conjunto de dicionários  em  uma  lista,  uma  lista  de  itens  em  um  dicionário  ou  até mesmo  um  dicionário  em  outro  dicionário. 
 
 Uma lista de dicionários
-Como podemos administrar uma frota de alienígenas? Uma maneira é criar uma lista de alienígenas, em que cada  alienígena  seja  representado  por  um  dicionário  com  informações sobre  ele.  Por  exemplo,  o  código  a  seguir  cria  uma  lista  com  três alienígenas:"""
+    Como podemos administrar uma frota de alienígenas? Uma maneira é criar uma lista de alienígenas, em que cada  alienígena  seja  representado  por  um  dicionário  com  informações sobre  ele.  Por  exemplo,  o  código  a  seguir  cria  uma  lista  com  três alienígenas:"""
 
 print("Exemplo 1")
 alien_0 = {'color': 'green', 'points': 5}
@@ -14,7 +15,8 @@ aliens = [alien_0, alien_1, alien_2]
 for alien in aliens:
     print(alien)
 
-""" No exemplo a seguir, usamos range() para criar uma frota de 10 alienígenas:"""
+""" 
+    No exemplo a seguir, usamos range() para criar uma frota de 10 alienígenas:"""
 
 print("\nExemplo 2")
 aliens = []
@@ -26,7 +28,8 @@ for alien_num in range(10):
 print(aliens[:3])
 print(f"Total de aliens: {len(aliens)}")
 
-"""Como podemos trabalhar com um conjunto de alienígenas como esse? Suponha  que  um  aspecto  do  jogo  consista  em  fazer  alguns  alienígenas mudarem  de  cor  e  moverem-se  mais  rápido  à  medida  que  o  jogo prosseguir. Quando for o momento de mudar de cor, podemos usar um laço for e uma instrução if para alterar a cor dos alienígenas"""
+"""
+    Como podemos trabalhar com um conjunto de alienígenas como esse? Suponha  que  um  aspecto  do  jogo  consista  em  fazer  alguns  alienígenas mudarem  de  cor  e  moverem-se  mais  rápido  à  medida  que  o  jogo prosseguir. Quando for o momento de mudar de cor, podemos usar um laço for e uma instrução if para alterar a cor dos alienígenas"""
 
 print("\nExemplo 3")
 for alien in aliens[:4]:
@@ -38,9 +41,9 @@ for alien in aliens[:4]:
 for alien in aliens:
     print(alien)
 
-"""Uma lista em um dicionário
-Em vez de colocar um dicionário em uma lista, às vezes é conveniente colocar  uma  lista  em  um  dicionário.  Por  exemplo,  considere  como podemos  descrever  uma  pizza  que  alguém  está  pedindo.  Se  usássemos
-apenas uma lista, tudo que poderíamos realmente armazenar é uma lista dos ingredientes da pizza. Com um dicionário, uma lista de ingredientes pode ser apenas um dos aspectos da pizza que estamos descrevendo"""
+"""
+Uma lista em um dicionário
+    Em vez de colocar um dicionário em uma lista, às vezes é conveniente colocar uma lista em um dicionário. Por  exemplo, considere como podemos descrever uma pizza que alguém está pedindo. Se usássemos apenas uma lista, tudo que poderíamos realmente armazenar é uma lista dos ingredientes da pizza. Com um dicionário, uma lista de ingredientes pode ser apenas um dos aspectos da pizza que estamos descrevendo"""
 
 print("\nExemplo 4")
 pizza = {
@@ -53,8 +56,9 @@ print(f"O pedido é de uma borda {pizza["borda"]}, e com acrescimo de: ")
 for acrescimo in pizza["acrescimos"]:
     print(f"\t {acrescimo}")
 
-"""Um dicionário em um dicionário
-Podemos aninhar um dicionário em outro dicionário, mas o código poderá ficar complicado rapidamente se isso for feito. Por exemplo, se você tiver vários usuários em um site, cada um com um nome único, os nomes dos usuários poderão ser usados como chaves em um dicionário. Você poderá então armazenar informações sobre cada usuário usando um dicionário como o valor associado a cada nome de usuário."""
+"""
+Um dicionário em um dicionário
+    Podemos aninhar um dicionário em outro dicionário, mas o código poderá ficar complicado rapidamente se isso for feito. Por exemplo, se você tiver vários usuários em um site, cada um com um nome único, os nomes dos usuários poderão ser usados como chaves em um dicionário. Você poderá então armazenar informações sobre cada usuário usando um dicionário como o valor associado a cada nome de usuário."""
 
 print("\nExemplo 5")
 users = {
@@ -69,7 +73,6 @@ users = {
         'last': 'curie',
         'location': 'paris',
         },
- 
     }
 
 for user, user_info in users.items():
